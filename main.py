ea6dfd73a60770e2dc5f1eaf4aad71354384980e
Toru Ebihara <ebihara@PMR543N.primo.mamezou.com>
2023-10-12 22:16:37 +0900

    ui commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.markdown("""
+    <style>
+        .title {
+            font = "sans-serif";
+            font-size:60px !important;
+            font-weight: bold;
+        }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.markdown('<div class="title">COE-AI Chatbot</div>', unsafe_allow_html=True)
+
+st.write("xxxxx")
