a705b11eda9710f6d67d9b074ede18fcf87e3881
Toru Ebihara <ebihara@PMR543N.primo.mamezou.com>
2023-10-15 16:08:48 +0900

    test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.markdown("""
     <style>
@@ -13,3 +14,10 @@ st.markdown("""
 st.markdown('<div class="title">COE-AI Chatbot</div>', unsafe_allow_html=True)
 
 st.write("xxxxx")
+
+response = requests.get("http://localhost:8000/api")
+
+if response.status_code == 200:
+    st.write("FastAPI says:", response.json()["message"])
+else:
+    st.write("Failed to fetch data from FastAPI")
