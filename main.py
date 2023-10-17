ce5649b9a590b37a97fae23e8beb4132a981b572
Toru Ebihara <ebihara@PMR543N.primo.mamezou.com>
2023-10-17 11:51:41 +0900

    test open ai

--- main.py
+++ main.py
@@ -121,29 +121,6 @@ if st.session_state.prev_selection != selected_prototype + selected_prototype2 :
 # プロンプトを元にAIの回答を生成する
 #------------------------------------------------------------------------
 
-#--------------------------------------------------------------
-
-# #Note: The openai-python library support for Azure OpenAI is in preview.
-# import os
-# import openai
-# openai.api_type = "azure"
-# openai.api_base = "https://testopennetwork.openai.azure.com/"
-# openai.api_version = "2023-07-01-preview"
-# openai.api_key = "fc9f375146314c79b2e30139e15240a2"
-
-# response = openai.ChatCompletion.create(
-#   engine="gpt-35-turbo2",
-#   messages = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":"あなたの誕生日は"}],
-#   temperature=0.7,
-#   max_tokens=800,
-#   top_p=0.95,
-#   frequency_penalty=0,
-#   presence_penalty=0,
-#   stop=None)
-
-  
-#--------------------------------------------------------------
-
 # アプリの再実行の際に履歴のチャットメッセージを表示
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -159,14 +136,12 @@ if prompt := st.chat_input("Please enter the prompt"):
 
     with st.spinner('回答を生成中...'):
         openai.api_type = "azure"
-        # openai.api_base = "https://openai-mz-chatbot.openai.azure.com/"
-        openai.api_base = "https://testopennetwork.openai.azure.com/"
+        openai.api_base = "https://chatbot-ai-ebihara-public.openai.azure.com/"
         openai.api_version = "2023-07-01-preview"
-        # openai.api_key = "031c7e70e64744d0b4aca4861184163d"
-        openai.api_key = "fc9f375146314c79b2e30139e15240a2"
+        openai.api_key = os.getenv("OPENAI_API_KEY")
 
         response = openai.ChatCompletion.create(
-        engine="gpt-35-turbo2",
+        engine="gpt-35-turbo",
         messages = [{"role":"system","content":"You are an AI assistant that helps people find information."},{"role":"user","content":prompt}],
         temperature=0.7,
         max_tokens=800,
@@ -174,12 +149,11 @@ if prompt := st.chat_input("Please enter the prompt"):
         frequency_penalty=0,
         presence_penalty=0,
         stop=None)
-        message_content = response['choices'][0]['message']['content']
-        st.chat_message("assistant").markdown(message_content)
-        st.session_state.messages.append({"role": "assistant", "content": message_content})
 
-    if selected_prototype != selected_prototype2:
-        with st.spinner('回答を生成中...'):
-            response2 = selected_prototype2  + "  \n" + prompt + "→xxxx2"
-            st.chat_message("assistant").markdown(response2)
-            st.session_state.messages.append({"role": "assistant", "content": response2})
+    if selected_prototype == selected_prototype2:
+        st.stop()
+
+    with st.spinner('回答を生成中...'):
+        response2 = selected_prototype2  + "  \n" + prompt + "→xxxx2"
+        st.chat_message("assistant").markdown(response2)
+        st.session_state.messages.append({"role": "assistant", "content": response2})
