e4d21698694eec680b79649651a9a077ad12520f
Toru Ebihara <ebihara@PMR543N.primo.mamezou.com>
2023-11-03 18:30:05 +0900

    DB入れ替えに伴う利用者の更新処理を追加

--- prototype/blob_db_download.py
+++ prototype/blob_db_download.py
@@ -7,37 +7,36 @@ BlobからDBデータをDLして同データを元に回答を生成する
 
 """
 import os
-import sys
 
-sys.path.append(os.path.abspath("prototype"))
 from prototype_base import PrototypeBase
 from azure.storage.blob import BlobServiceClient
 
-class PrototypeSample(PrototypeBase):
+class Prototype(PrototypeBase):
 
     @PrototypeBase.processing_structure
-    def run(self, params):
+    def run(params : PrototypeBase.Params):
         """
-        プロトタイプ実行
+        プロトタイプ
 
+        Args:
+        入力パラメータ群
         Returns:
-        プロンプトへのAI回答
-
+        プロンプトに対するAIの回答
         """
-        connection_string = "DefaultEndpointsProtocol=https;AccountName=mzbotstorage;AccountKey=55enF2UEzMTFtjn9mpg9TPvSgNpWgULSLkj0U3ErPYZhjwoNnQQoRoW6JzszuHO31h+sz0P2XaZ8+ASt0rOc+Q==;EndpointSuffix=core.windows.net"  # Azure Portal から取得する
+        connection_string = "DefaultEndpointsProtocol=https;AccountName=mzbotstorage;AccountKey=55enF2UEzMTFtjn9mpg9TPvSgNpWgULSLkj0U3ErPYZhjwoNnQQoRoW6JzszuHO31h+sz0P2XaZ8+ASt0rOc+Q==;EndpointSuffix=core.windows.net"
         container_name = "root"
-        blob_name = "test.txt"
-        download_path = "./test.txt"
+        blob_name = params.config["db_path"]
+        download_path = "./DB/chroma.sqlite3"
 
         blob_service_client = BlobServiceClient.from_connection_string(connection_string)
         blob_client = blob_service_client.get_blob_client(container=container_name, blob=blob_name)
-        directory = os.path.dirname(download_path)
+        directory = "DB"
         
         print(blob_client)
-        if not os.path.exists(directory):
+        if not os.path.exists("DB"):
             os.makedirs(directory)
         with open(download_path, "wb") as download_file:
             download_file.write(blob_client.download_blob().readall())
 
 if __name__ == "__main__":
-    PrototypeSample().run()
+    Prototype().run()
